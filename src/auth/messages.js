f0a916ff0d90aac65df0cf61effcc844bd9011be
Mohammed <Mohammed.ofisan@hotmail.com>
2019-12-18 00:51:31 +0300

    added messages

--- src/auth/messages.js
+++ src/auth/messages.js
@@ -3,9 +3,15 @@ export default {
   signUpFailure: 'Email address is already used, or check your confirmation pasword to match it with password',
   signInSuccess: 'Welcome!',
   signInFailure: 'Failed to sign in. Check your email and password and try' +
-                  ' again.',
+    ' again.',
   signOutSuccess: 'Come back soon!',
   changePasswordSuccess: 'Password changed successfully!',
   changePasswordFailure: 'Failed to change passwords. Check your old password' +
-                         ' and try again.'
+    ' and try again.',
+  createBlogFailure: "Failed to create Blog.",
+  createBlogSuccess: "Succefully crearted Blog.",
+  createConsultationFailure: "Failed to create Consultatio.",
+  createConsultationSuccess: "Succefully crearted Consultatio.",
+  createTypeOfConsultationFailure: "Failed to create Type Of Consultation.",
+  createTypeOfConsultationSuccess: "Succefully crearted Type Of Consultation."
 }
