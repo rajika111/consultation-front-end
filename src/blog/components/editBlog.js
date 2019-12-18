86c42cedb930df0ab5ea26142a9edcff7107ad6a
Mohammed <Mohammed.ofisan@hotmail.com>
2019-12-18 16:41:04 +0300

    added edit blog

--- src/blog/components/editBlog.js
+++ src/blog/components/editBlog.js
@@ -1,68 +1,85 @@
 import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
-
-import { changeBlog } from '../api'
-import messages from '../messages'
-
-class ChangeBlog extends Component {
+import { Link, Redirect } from 'react-router-dom';
+import { editBlogById } from '../../auth/api'
+import messages from '../../auth/messages'
+class EditBlog extends Component {
     constructor() {
         super()
-
         this.state = {
             title: '',
             content: '',
             author: ''
         }
     }
+    componentDidMount() {
+        const { info } = this.props.location.state
+        console.log(this.props);
 
+        let title = info.title
+        let content = info.content
+        let author = info.author
+
+        let id = info._id
+        this.setState({
+            title, content, author, id
+        })
+    }
     handleChange = event => this.setState({
         [event.target.name]: event.target.value
     })
-
-    onChangeBlog = event => {
+    onEditBlog = event => {
         event.preventDefault()
-
         const { alert, history, user } = this.props
-
-        changeBlog(this.state, user)
-            .then(() => alert(messages.changeBlogSuccess, 'success'))
-            .then(() => history.push('/'))
+        editBlogById(this.state, this.props.user)
+            .then((response) => { })
+            .then(() => alert(messages.editBlogSuccess, 'success'))
+            .then(() => history.push('/blog'))
             .catch(error => {
-                console.error(error)
-                this.setState({ oldBlog: '', newBlog: '' })
-                alert(messages.changeBlogFailure, 'danger')
+                console.log(error)
+                this.setState({
+                    title: '',
+                    content: '',
+                    author: '',
+                })
+                alert(messages.editBlogFailure, 'danger')
             })
     }
 
     render() {
-        const { oldBlog, newBlog } = this.state
-
+        const { title, content, author } = this.state
         return (
-            <form className='auth-form' onSubmit={this.onChangeBlog}>
-                <h3>Change Blog</h3>
-
-                <label htmlFor="oldpw">Old Blog</label>
+            <form className='auth-form' onSubmit={this.onEditBlog}>
+                <h3>Edit A Blog</h3>
+                <label htmlFor="Title">Title</label>
                 <input
                     required
-                    name="oldBlog"
-                    value={oldBlog}
-                    type="plog"
-                    placeholder="Old Blog"
+                    name="title"
+                    value={title}
+                    type="title"
+                    placeholder="Title"
                     onChange={this.handleChange}
                 />
-                <label htmlFor="newBlog">New Blog</label>
+                <label htmlFor="content">Content</label>
                 <input
                     required
-                    name="newBlog"
-                    value={newBlog}
-                    type="plog"
-                    placeholder="New Blog"
+                    name="content"
+                    value={content}
+                    type="content"
+                    placeholder="Content"
                     onChange={this.handleChange}
                 />
-                <button type="submit">Change Blog</button>
+                <label htmlFor="author">Author</label>
+                <input
+                    required
+                    name="author"
+                    value={author}
+                    type="author"
+                    placeholder="Author"
+                    onChange={this.handleChange}
+                />
+                <button type="submit">ADD Blog</button>
             </form>
         )
     }
 }
-
-export default withRouter(ChangeBlog)
+export default EditBlog;
